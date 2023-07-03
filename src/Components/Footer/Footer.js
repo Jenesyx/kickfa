1b83cf7f517ab0624851322f796b974dd2b53f9a
Jenesyx <a.bidkhori2004@gmail.com>
2023-07-03 23:27:07 +0200

    1.2.0 - Changed all colors and header, footer, icons image

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,13 @@ function Footer() {
   return (
     <>
         <div className='Footer'>
+          <div className='footer-content-holder wrap'>
+            <div className='left'></div>
+            <div className='right'></div>
+          </div>
+          <div className='copyright wrap'>
+            <p>© 2022 KickFa. All rights reserved.</p>
+          </div>
         </div>
     </>
   )
